6c516aa8510d21fc18f6e869e2380fef89b23ba3
Aaron Na <syn228@nyu.edu>
2018-05-01 19:16:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,6 +18,14 @@ class Student
     # remember each row should be a new instance of the Student class
   end
 
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(name) FROM students WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
     SELECT * FROM students WHERE name = ? LIMIT 1
