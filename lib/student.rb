69c4e7c3d3b252f96296105ea4d1ecf4a69d55d2
Aaron Na <syn228@nyu.edu>
2018-05-01 19:12:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,7 @@ class Student
     SQL
     
     DB[:conn].execute(sql, name).map do |row|
-      self.new
+      self.new_from_db(row)
     
     # find the student in the database given a name
     # return a new instance of the Student class
