e90638f79df57546d16213fb6026d4c72dcb7669
Aaron Na <syn228@nyu.edu>
2018-05-01 17:59:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,11 +2,11 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    new_song = self.new
-    new_song.id = row[0]
-    new_song.name = row[1]
-    new_song.grade = row[2]
-    new_song
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
