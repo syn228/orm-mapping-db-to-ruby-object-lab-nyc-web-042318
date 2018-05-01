e0fa41e9b3cbaa00e67f3cda49e69fee9e3a305d
Aaron Na <syn228@nyu.edu>
2018-05-01 19:19:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,7 @@ class Student
       SELECT * 
       FROM students
     SQL
+    DB[:conn].execute(sql)
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
