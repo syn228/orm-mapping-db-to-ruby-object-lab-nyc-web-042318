36021d4afee5099894a107d7347e7050e78456d2
Aaron Na <syn228@nyu.edu>
2018-05-01 19:35:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -23,9 +23,9 @@ class Student
   end
   def self.all_students_in_grade_X(int)
     sql = <<-SQL
-    SELECT * FROM students WHERE grade = int
+    SELECT * FROM students WHERE grade = ?
       SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, int).map do |row|
       self.new_from_db(row)
     end
   end
