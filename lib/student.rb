d02184e8a878e3b9e8403fb6ade2301ef8b53479
Aaron Na <syn228@nyu.edu>
2018-05-01 19:30:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,15 @@ class Student
       self.new_from_db(row)
     end.first(int)
   end
+  
+    def self.first_X_students_in_grade_10
+    sql = <<-SQL
+    SELECT * FROM students WHERE grade = 10
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
